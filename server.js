7302d84f5dd74b37f3805f804780b6e365bcae31
Eduard Hovhannisyan <66397209+eduard0497@users.noreply.github.com>
2023-06-14 15:50:54 -0700

    another submit by Ed

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 This is auto generated boilerplate, we can remove it or modify it
 -Drew
 */
-'use strict';
-var http = require('http');
+"use strict";
+var http = require("http");
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+  .listen(port);
+
+// comment by Ed
