f57ab3a4c7838a34bcdb54dd11c4ded1065f4718
Eduard Hovhannisyan <66397209+eduard0497@users.noreply.github.com>
2023-06-15 19:46:11 -0700

    submitting completed user-register and user-verify-email endpoints

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const knex = require("knex");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+//
+const _EMAIL = process.env.EMAIL;
+const _PASSWORD = process.env.PASSWORD;
 const saltRounds = 5;
 const randomstring = require("randomstring");
 
@@ -19,6 +24,34 @@ const database = knex({
     database: "csun_task_force",
   },
 });
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: _EMAIL,
+    pass: _PASSWORD,
+  },
+});
+
+const mailOptions = (sendTo, subject, text) => {
+  var obj = {
+    from: _EMAIL,
+    to: sendTo,
+    subject: subject,
+    text: text,
+  };
+  return obj;
+};
+
+const sendEmail = (itemToMail) => {
+  transporter.sendMail(itemToMail, (error, info) => {
+    if (error) {
+      console.log("Sending Email error");
+    } else {
+      console.log(info.response);
+    }
+  });
+};
 
 app.post("/user-register", async (req, res) => {
   const { fname, lname, email, password } = req.body;
@@ -42,14 +75,56 @@ app.post("/user-register", async (req, res) => {
         email_active: false,
         access_token: initialRandomString,
       })
-      .then((data) => {
+      .then(async (data) => {
         if (data.length == 0) {
           res.json("error occured, unable to add the user");
         } else {
+          let constructedLink = `http://localhost:4000/user-verify-email?user_id=${data[0].id}&fname=${data[0].fname}&lname=${data[0].lname}&access_token=${data[0].access_token}`;
+          let objectThatHasMailOptions = mailOptions(
+            email,
+            "Email Verification",
+            `Please click the link to verify: ${constructedLink}`
+          );
+          await sendEmail(objectThatHasMailOptions);
           res.json("user added successfully");
         }
       });
   }
+});
+
+app.get("/user-verify-email", (req, res) => {
+  const { user_id, fname, lname, access_token } = req.query;
+
+  database("users")
+    .select("*")
+    .where({
+      id: user_id,
+      fname,
+      lname,
+      access_token,
+    })
+    .then(async (data) => {
+      if (data.length != 1) {
+        res.json("Unable to find the user");
+      } else if (data[0].email_active) {
+        res.json("Email is already active");
+      } else {
+        database("users")
+          .returning("*")
+          .update({ email_active: true })
+          .where({
+            id: user_id,
+            fname,
+            lname,
+            access_token,
+          })
+          .then(() => res.json("Email has been activated"));
+      }
+    });
+});
+
+app.post("/user-login", (req, res) => {
+  // not done yet
 });
 
 app.listen(4000, () => {
